7c2b0d968117763abe46e0c68ecb5b814ff3eba5
shouta0715 <Myheroes0715@gmail.com>
2024-08-25 20:00:14 +0900

    delete: trash

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,14 +3,12 @@ import { MultiStep } from "@/features/multi-steps";
 import { AppCards } from "@/features/the-basics/app-cards";
 import { AppStore } from "@/features/the-basics/app-store";
 import { FeedBack } from "@/features/the-basics/feed-back";
-import { Trash } from "@/features/trash";
 import { buttonVariants } from "@/features/ui/button";
 import { cn } from "@/lib/utils";
 
 export default function Home() {
   return (
     <div>
-      <Trash />
       <AppCards />
       <div className="mt-10">
         <AppStore />
